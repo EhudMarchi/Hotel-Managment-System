2e427debb69d63c8f763580848e1e7984c4deb71
Ehud <marchiehud@gmail.com>
2020-06-02 20:14:07 +0300

    Payement Screen added

--- src/HotelManagementUIview/AvailableRoomsScreen.java
+++ src/HotelManagementUIview/AvailableRoomsScreen.java
@@ -24,6 +24,7 @@ public class AvailableRoomsScreen extends JFrame {
         deluxeLabel.setText("Available Deluxe Rooms:"+availableRooms.get(2));
         premiumLabel.setText("Available Premium Rooms:"+availableRooms.get(3));
         suiteLabel.setText("Available Suite Rooms:"+availableRooms.get(4));
+
     }
 
     public AvailableRoomsScreen() {
@@ -38,11 +39,6 @@ public class AvailableRoomsScreen extends JFrame {
         deluxeLabel = new JLabel();
         premiumLabel = new JLabel();
         suiteLabel = new JLabel();
-        twinSpinner = new JSpinner();
-        familySpinner = new JSpinner();
-        deluxeSpinner = new JSpinner();
-        premiumSpinner = new JSpinner();
-        suiteSpinner = new JSpinner();
         backgroundLabel = new JLabel();
 
         //======== this ========
@@ -54,85 +50,43 @@ public class AvailableRoomsScreen extends JFrame {
         dateslabel.setForeground(Color.white);
         dateslabel.setBackground(Color.black);
         dateslabel.setOpaque(true);
-        dateslabel.setFont(new Font("Segoe UI", Font.BOLD, 12));
         contentPane.add(dateslabel);
-        dateslabel.setBounds(260, 80, 290, 25);
+        dateslabel.setBounds(250, 20, 270, 25);
 
         //---- twinLabel ----
         twinLabel.setText("Available Twin Rooms:");
         twinLabel.setOpaque(true);
         twinLabel.setBackground(Color.black);
-        twinLabel.setForeground(Color.white);
-        twinLabel.setFont(new Font("Segoe UI", Font.BOLD, 12));
         contentPane.add(twinLabel);
-        twinLabel.setBounds(200, 135, 225, 35);
+        twinLabel.setBounds(190, 75, 385, 35);
 
         //---- familyLabel ----
         familyLabel.setText("Available Family Rooms:");
         familyLabel.setOpaque(true);
         familyLabel.setBackground(Color.black);
-        familyLabel.setForeground(Color.white);
-        familyLabel.setFont(new Font("Segoe UI", Font.BOLD, 12));
         contentPane.add(familyLabel);
-        familyLabel.setBounds(200, 175, 225, 35);
+        familyLabel.setBounds(190, 115, 385, 35);
 
         //---- deluxeLabel ----
         deluxeLabel.setText("Available Deluxe Rooms:");
         deluxeLabel.setOpaque(true);
         deluxeLabel.setBackground(Color.black);
-        deluxeLabel.setForeground(Color.white);
-        deluxeLabel.setFont(new Font("Segoe UI", Font.BOLD, 12));
         contentPane.add(deluxeLabel);
-        deluxeLabel.setBounds(200, 215, 225, 35);
+        deluxeLabel.setBounds(190, 155, 385, 35);
 
         //---- premiumLabel ----
         premiumLabel.setText("Available Premium Rooms:");
         premiumLabel.setOpaque(true);
         premiumLabel.setBackground(Color.black);
-        premiumLabel.setForeground(Color.white);
-        premiumLabel.setFont(new Font("Segoe UI", Font.BOLD, 12));
         contentPane.add(premiumLabel);
-        premiumLabel.setBounds(200, 255, 225, 35);
+        premiumLabel.setBounds(190, 195, 385, 35);
 
         //---- suiteLabel ----
         suiteLabel.setText("Available Suite Rooms:");
         suiteLabel.setOpaque(true);
         suiteLabel.setBackground(Color.black);
-        suiteLabel.setForeground(Color.white);
-        suiteLabel.setFont(new Font("Segoe UI", Font.BOLD, 12));
         contentPane.add(suiteLabel);
-        suiteLabel.setBounds(200, 295, 225, 35);
-
-        //---- twinSpinner ----
-        twinSpinner.setBackground(Color.black);
-        twinSpinner.setModel(new SpinnerNumberModel(0, 0, null, 1));
-        twinSpinner.setFont(twinSpinner.getFont().deriveFont(twinSpinner.getFont().getStyle() | Font.BOLD));
-        contentPane.add(twinSpinner);
-        twinSpinner.setBounds(510, 135, 50, 35);
-
-        //---- familySpinner ----
-        familySpinner.setModel(new SpinnerNumberModel(0, 0, null, 1));
-        familySpinner.setFont(familySpinner.getFont().deriveFont(familySpinner.getFont().getStyle() | Font.BOLD));
-        contentPane.add(familySpinner);
-        familySpinner.setBounds(510, 175, 50, 35);
-
-        //---- deluxeSpinner ----
-        deluxeSpinner.setFont(deluxeSpinner.getFont().deriveFont(deluxeSpinner.getFont().getStyle() | Font.BOLD));
-        deluxeSpinner.setModel(new SpinnerNumberModel(0, 0, null, 1));
-        contentPane.add(deluxeSpinner);
-        deluxeSpinner.setBounds(510, 215, 50, 35);
-
-        //---- premiumSpinner ----
-        premiumSpinner.setFont(premiumSpinner.getFont().deriveFont(premiumSpinner.getFont().getStyle() | Font.BOLD));
-        premiumSpinner.setModel(new SpinnerNumberModel(0, 0, null, 1));
-        contentPane.add(premiumSpinner);
-        premiumSpinner.setBounds(510, 255, 50, 35);
-
-        //---- suiteSpinner ----
-        suiteSpinner.setFont(suiteSpinner.getFont().deriveFont(suiteSpinner.getFont().getStyle() | Font.BOLD));
-        suiteSpinner.setModel(new SpinnerNumberModel(0, 0, null, 1));
-        contentPane.add(suiteSpinner);
-        suiteSpinner.setBounds(510, 295, 50, 35);
+        suiteLabel.setBounds(190, 235, 385, 35);
 
         //---- backgroundLabel ----
         backgroundLabel.setIcon(new ImageIcon(getClass().getResource("/MainScreenBackground.png")));
@@ -166,11 +120,6 @@ public class AvailableRoomsScreen extends JFrame {
     private JLabel deluxeLabel;
     private JLabel premiumLabel;
     private JLabel suiteLabel;
-    private JSpinner twinSpinner;
-    private JSpinner familySpinner;
-    private JSpinner deluxeSpinner;
-    private JSpinner premiumSpinner;
-    private JSpinner suiteSpinner;
     private JLabel backgroundLabel;
     // JFormDesigner - End of variables declaration  //GEN-END:variables
 }
